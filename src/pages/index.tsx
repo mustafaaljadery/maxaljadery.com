6b8c3e29bb23a954bfbeaf05633616b7b5cf62bc
Mustafa Aljadery <mustafaaljadery1@gmail.com>
2023-05-26 08:47:08 -0700

    adds

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,36 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center">
       <header className="w-full flex flex-row ">
-        <div></div>
+        <div className="flex flex-row space-x-1">
+          <p>Max Aljadery</p>
+        </div>
         <div className="flex flex-row space-x-3">
-          <a>Twitter</a>
-          <a>Linkedin</a>
-          <a></a>
+          <Link href="/blog">
+            <a className="">Blog</a>
+          </Link>
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-xs silka-semibold text-gray-500"
+          >
+            Twitter
+          </a>
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+            className=""
+          >
+            Linkedin
+          </a>
         </div>
       </header>
-      <div className=""></div>
+      <div className="py-24 flex flex-col"></div>
       <footer></footer>
     </main>
   );
