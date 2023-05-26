6aad737bce066ebf85c0dcd684ea05a456fca393
Mustafa Aljadery <mustafaaljadery1@gmail.com>
2023-05-26 08:40:34 -0700

    ads

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main className="flex flex-col justify-center items-center">
+      <header className="w-full flex flex-row ">
+        <div></div>
+        <div className="flex flex-row space-x-3">
+          <a>Twitter</a>
+          <a>Linkedin</a>
+          <a></a>
+        </div>
+      </header>
+      <div className=""></div>
+      <footer></footer>
+    </main>
+  );
+}
