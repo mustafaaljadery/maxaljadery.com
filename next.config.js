96101de664349d3d09ae927ac40e8361b673f8a6
Mustafa Aljadery <mustafaaljadery1@gmail.com>
2023-08-09 19:25:19 -0700

    redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,51 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/lora',
+        destination:
+          'https://github.com/mustafaaljadery/lora-fine-tune',
+        permanent: true,
+      },
+      {
+        source: '/nn',
+        destination:
+          'https://github.com/mustafaaljadery/deep-nn-scratch',
+        permanent: true,
+      },
+      {
+        source: '/search',
+        destination:
+          'https://github.com/mustafaaljadery/paul-graham-essays-search',
+        permanent: true,
+      },
+      {
+        source: '/text-editor',
+        destination:
+          'https://github.com/mustafaaljadery/collaborative-texteditor',
+        permanent: true,
+      },
+      {
+        source: '/cybersyn',
+        destination: 'https://cybersyn.maxaljadery.com',
+        permanent: true,
+      },
+      {
+        source: '/blog-generate',
+        destination:
+          'https://github.com/mustafaaljadery/blog-generator',
+        permanent: true,
+      },
+      {
+        source: '/randomization',
+        destination:
+          'https://github.com/mustafaaljadery/Randomization-Interface-for-Experiment-Design',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
